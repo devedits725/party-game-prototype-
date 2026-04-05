9043693e0dfb716562c511cd000906acd56be842
Dev_raheja <devedits725@gmail.com>
2026-04-05 23:12:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,8 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/host/:game/:mode" element={<HostLobby />} />
-=======
->>>>>>> 229d39f7510f794d97ecf01b8a41be9139875e08
-        <Route path="/host/:game" element={<HostLobby />} />
+<Route path="/host/:game/:mode" element={<HostLobby />} />
+<Route path="/host/:game" element={<HostLobby />} />
         <Route path="/join/:code" element={<JoinPage />} />
         <Route path="/join" element={<JoinPage />} />
       </Routes>
